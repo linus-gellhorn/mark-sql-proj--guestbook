d315bed931d9b187a2b382f177a7abcc8a70c92e
Linus Gellhorn <linus.g@hotmail.com>
2021-12-01 23:31:00 +0000

    Provided heroku db via env

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,37 @@ import express from "express";
 import cors from "cors";
 import { Client } from "pg";
 
-//As your database is on your local machine, with default port,
-//and default username and password,
-//we only need to specify the (non-default) database name.
+//This line will read in any MY_KEY=myValue pairs in your .env file and
+// make them available as environment variables as properties of process.env
+// Example: if the file has
+// MY_KEY=myValue
+// we'd be able to access process.env.MY_KEY
+// Specifically, you should provide a DB connection string as DATABASE_URL in .env
+require("dotenv").config();
 
-const client = new Client({ database: "guestbook" });
+if (!process.env.DATABASE_URL) {
+  throw "No DATABASE_URL env var!  Have you made a .env file?  And set up dotenv?";
+}
 
-//TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
+// To connect to a heroku db you need to specify an object value for the ssl option
+// (however, if you want to connect to a local db you should set this property to false).
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 client.connect();
+
+// //As your database is on your local machine, with default port,
+// //and default username and password,
+// //we only need to specify the (non-default) database name.
+
+// const client = new Client({ database: "guestbook" });
+
+// //TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
+// client.connect();
 
 const app = express();
 
