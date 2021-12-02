daa87c39d8270875eea4f8c1c154b35972508685
Linus Gellhorn <linus.g@hotmail.com>
2021-12-02 17:22:13 +0000

    Used npm to install dotenv

--- dist/server.js
+++ dist/server.js
@@ -15,12 +15,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const pg_1 = require("pg");
-//As your database is on your local machine, with default port,
-//and default username and password,
-//we only need to specify the (non-default) database name.
-const client = new pg_1.Client({ database: "guestbook" });
-//TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
+//This line will read in any MY_KEY=myValue pairs in your .env file and
+// make them available as environment variables as properties of process.env
+// Example: if the file has
+// MY_KEY=myValue
+// we'd be able to access process.env.MY_KEY
+// Specifically, you should provide a DB connection string as DATABASE_URL in .env
+require("dotenv").config();
+if (!process.env.DATABASE_URL) {
+    throw "No DATABASE_URL env var!  Have you made a .env file?  And set up dotenv?";
+}
+// To connect to a heroku db you need to specify an object value for the ssl option
+// (however, if you want to connect to a local db you should set this property to false).
+const client = new pg_1.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 client.connect();
+// //As your database is on your local machine, with default port,
+// //and default username and password,
+// //we only need to specify the (non-default) database name.
+// const client = new Client({ database: "guestbook" });
+// //TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
+// client.connect();
 const app = express_1.default();
 /**
  * Simplest way to connect a front-end. Unimportant detail right now, although you can read more: https://flaviocopes.com/express-cors/
